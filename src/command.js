54efbfe0c07b544511e38151f1454dbc0414a38b
Cherry <cherryred5959@gmail.com>
2018-12-31 06:49:30 +0900

    구조 변경 임시 커밋

---
+++ src/command.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { Signale } = require('signale');
+const option = require('./prompts/option');
+const browser = require('./puppeteer/browser');
+
+module.exports = async () => {
+    const signale = new Signale();
+
+    try {
+        switch (await option()) {
+            case 'install':
+                break;
+
+
+        }
+
+        //     await puppeteer(values);
+
+    } catch (error) {
+        signale.fatal(error);
+    }
+};
---
+++ src/prompts/option.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { Select } = require('enquirer');
+
+module.exports = async () => {
+    const prompt = new Select({
+        name: 'option',
+        message: 'What do you want to start?',
+        choices: [
+            'install',
+            'fresh',
+            'download',
+            'clear',
+        ],
+    });
+
+    return await prompt.run();
+};
